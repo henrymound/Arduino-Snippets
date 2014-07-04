e0d8d1aaa77e73686a4be0485c4a2f90d9e13cff
henrymound <henrymound@gmail.com>
2014-07-04 12:34:31 -0400

    Main method made static

--- Hue_Converter/Hue_Converter.ino
+++ Hue_Converter/Hue_Converter.ino
@@ -4,26 +4,10 @@ void setup() {
 
 void loop() { 
 
- toRGB(341);
- toRGB(830);
- toRGB(1029);
- toRGB(439);
- toRGB(873);
-
-}
-
-String toRGB(int hue){
-  /*Converts the int hue to three different ints in an array
-  the first int is red, second - green, third - blue each on a 255 scale*/
-  int red, green, blue;
-  
-  if(hue >= 0 && hue <= 255){red = 255; green = 0; blue = 255 - hue;}
-  else if (hue >= 256 && hue <= 510){red = 255; green = hue - 255; blue = 0;}
-  else if (hue >= 511 && hue <= 765){red = 765 - hue; green = 255; blue = 0;}
-  else if (hue >= 766 && hue <= 1020){red = 0; green = 255; blue = hue - 765;}
-  else if (hue >= 1021 && hue <= 1275){red = 0; green = 1275 - hue; blue = 255;}
-  else if (hue >= 1276 && hue <= 1530){red = hue - 1275; green = 0; blue = 255;}
-  
+ int red, green, blue;
+ int hue = 830;
+ toRGB(hue, red, green, blue);
+ 
   Serial.print(hue);
   Serial.print(": ");
   Serial.print(red);
@@ -31,5 +15,24 @@ String toRGB(int hue){
   Serial.print(green);
   Serial.print(", ");
   Serial.println(blue);
+  
+// toRGB(830);
+// toRGB(1029);
+// toRGB(439);
+// toRGB(873);
+
+}
+
+void static toRGB(int hue, int red, int green, int blue){
+  /*Converts the int hue to three different ints in an array
+  the first int is red, second - green, third - blue each on a 255 scale*/
+  
+  if(hue >= 0 && hue <= 255){red = 255; green = 0; blue = 255 - hue;}
+  else if (hue >= 256 && hue <= 510){red = 255; green = hue - 255; blue = 0;}
+  else if (hue >= 511 && hue <= 765){red = 765 - hue; green = 255; blue = 0;}
+  else if (hue >= 766 && hue <= 1020){red = 0; green = 255; blue = hue - 765;}
+  else if (hue >= 1021 && hue <= 1275){red = 0; green = 1275 - hue; blue = 255;}
+  else if (hue >= 1276 && hue <= 1530){red = hue - 1275; green = 0; blue = 255;}
+
 
 }
