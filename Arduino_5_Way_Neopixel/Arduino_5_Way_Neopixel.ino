7dd90da7df84f0c8337421e9b6029a5153c29f0f
henrymound <henrymound@gmail.com>
2014-07-05 15:50:37 -0400

    Cleaned up + commented code

--- Arduino_5_Way_Neopixel/Arduino_5_Way_Neopixel.ino
+++ Arduino_5_Way_Neopixel/Arduino_5_Way_Neopixel.ino
@@ -1,15 +1,17 @@
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //Creates a Serial output for display
 }
 
 void loop() { 
 
+ //Converts a bunch of test numbers
  toRGB(234);
  toRGB(341);
  toRGB(830);
  toRGB(1029);
- toRGB(439);
- toRGB(873);
+ toRGB(439)
+ toRGB(823);
 
 }
 
@@ -25,6 +27,7 @@ String toRGB(int hue){
   else if (hue >= 1021 && hue <= 1275){red = 0; green = 1275 - hue; blue = 255;}
   else if (hue >= 1276 && hue <= 1530){red = hue - 1275; green = 0; blue = 255;}
   
+  //Prints out the resulting conversion
   Serial.print(hue);
   Serial.print(": ");
   Serial.print(red);
